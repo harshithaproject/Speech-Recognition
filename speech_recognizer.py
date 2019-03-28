e29e87587304c7c856e3e3cd96ac76173ed65d6e
harshithaproject <47668303+harshithaproject@users.noreply.github.com>
2019-03-28 07:51:32 +0530

    Add files via upload

---
+++ speech_recognizer.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+
+r=sr.Recognizer()
+
+with sr.Microphone() as source:
+    print ('Say Something!')
+    audio =r.listen(source)
+
+
+
+try:
+    print('Google thinks you said:\n' + r.recognize_google(audio))
+
+except:
+    pass
+    
